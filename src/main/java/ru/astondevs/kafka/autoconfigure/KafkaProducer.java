5e8955a97885ae92c6167d7842f5a246515c83fb
msyaskov <ms.yaskov@gmail.com>
2024-03-12 16:30:54 +0300

    docs: KafkaProducer

--- src/main/java/ru/astondevs/kafka/autoconfigure/KafkaProducer.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/KafkaProducer.java
@@ -7,11 +7,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация, которая помечает bean, как продюсера сообщений в kafka.
+ * Тип bean'а помеченного данной аннотацией должен расширять {@link AbstractKafkaProducer}.
+ *
+ * @author Максим Яськов
+ */
 @Component
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface KafkaProducer {
 
+    /**
+     * @return название конфигурации для данного продюсера
+     */
     String value() default "";
 
 }
