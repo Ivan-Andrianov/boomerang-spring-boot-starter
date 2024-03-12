2f55e443d5f815fd55909e3b375eac9ed0d53735
msyaskov <ms.yaskov@gmail.com>
2024-03-12 16:32:19 +0300

    docs: KafkaConsumer

--- src/main/java/ru/astondevs/kafka/autoconfigure/KafkaConsumer.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/KafkaConsumer.java
@@ -7,11 +7,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация, которая помечает bean, как потребителя сообщений из kafka.
+ * Тип bean'а помеченного данной аннотацией должен расширять {@link AbstractKafkaConsumer}.
+ *
+ * @author Максим Яськов
+ */
 @Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface KafkaConsumer {
 
+    /**
+     * @return название конфигурации для данного потребителя
+     */
     String value();
 
 }
