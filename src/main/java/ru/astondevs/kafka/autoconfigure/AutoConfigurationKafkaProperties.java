5f9d02bb82bc4f6e2a0270f096d61a3ed49990cf
msyaskov <ms.yaskov@gmail.com>
2024-03-12 18:13:55 +0300

    docs: AutoConfigurationKafkaProperties

--- src/main/java/ru/astondevs/kafka/autoconfigure/AutoConfigurationKafkaProperties.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/AutoConfigurationKafkaProperties.java
@@ -10,56 +10,119 @@ import org.springframework.util.unit.DataSize;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Свойства автоматической конфигурации.
+ *
+ * @author Ivan Andrianov
+ * @author Maksim Yaskov
+ */
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "ru.astondevs.kafka")
 public class AutoConfigurationKafkaProperties {
 
+    /**
+     * Карта названия конфигурации продюсера и свойства конфигурации продюсера.
+     */
     private Map<String, ProducerProperties> producers;
 
-    private Map<String, ConsumerProperties> consumers; // by groupId
+    /**
+     * Карта названия конфигурации потребителя и свойства конфигурации потребителя.
+     */
+    private Map<String, ConsumerProperties> consumers;
 
+    /**
+     * Свойства kafka продюсера.
+     */
     @Getter
     @Setter
     public static class ProducerProperties {
 
-        private String topic;
-
+        /**
+         * Размер пакета по умолчанию.
+         */
         private DataSize batchSize = DataSize.ofKilobytes(16);
 
-        private long lingerMs = 0;
-
+        /**
+         * Разделенный запятыми список пар хост:порт,
+         * которые будут использоваться для установления начальных подключений к кластеру Kafka.
+         */
         private List<String> bootstrapServers;
 
+        /**
+         * Название топика.
+         */
+        private String topic;
+
+        /**
+         * Задержка перед отправкой пакета (миллисекунды).
+         */
+        private long lingerMs = 0;
+
+        /**
+         * Класс серелизатора ключа.
+         */
         private Class<?> keySerializer = StringSerializer.class;
 
+        /**
+         * Название компонента являющегося серелизатором ключа.
+         */
+        private String keySerializerBeanName;
+
+        /**
+         * Класс серелизатора значения.
+         */
         private Class<?> valueSerializer = JsonSerializer.class;
+
+        /**
+         * Название компонента являющегося серелизатором значения.
+         */
+        private String valueSerializerBeanName;
 
     }
 
+    /**
+     * Свойства kafka потребителя.
+     */
     @Getter
     @Setter
     public static class ConsumerProperties {
 
-        private String topic;
-
-        private String groupId;
-
-        private DataSize batchSize = DataSize.ofKilobytes(16);
-
-        private long lingerMs = 0;
-
+        /**
+         * Разделенный запятыми список пар хост:порт,
+         * которые будут использоваться для установления начальных подключений к кластеру Kafka.
+         */
         private List<String> bootstrapServers;
 
+        /**
+         * Название топика.
+         */
+        private String topic;
+
+        /**
+         * Идентификатор группы потребителей.
+         */
+        private String groupId;
+
+        /**
+         * Класс серелизатора ключа.
+         */
         private Class<?> keySerializer = StringSerializer.class;
 
-        private Class<?> valueSerializer = JsonSerializer.class;
-
+        /**
+         * Название компонента являющегося серелизатором ключа.
+         */
         private String keyDeserializerBeanName;
 
+        /**
+         * Название компонента являющегося серелизатором значения.
+         */
         private String valueDeserializerBeanName;
 
+        /**
+         * Класс серелизатора значения.
+         */
+        private Class<?> valueSerializer = JsonSerializer.class;
+
     }
-
-
 }
