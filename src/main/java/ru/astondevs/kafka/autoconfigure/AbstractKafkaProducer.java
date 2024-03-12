9862f719ef5dcbcee12f69b4c1202717abc109ca
msyaskov <ms.yaskov@gmail.com>
2024-03-12 16:51:22 +0300

    docs: AbstractKafkaProducer

--- src/main/java/ru/astondevs/kafka/autoconfigure/AbstractKafkaProducer.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/AbstractKafkaProducer.java
@@ -17,24 +17,45 @@ import java.util.concurrent.CompletableFuture;
  */
 public abstract class AbstractKafkaProducer<K,V> implements InitializingBean {
 
+    /**
+     * Экземпляр KafkaTemplate специально созданный для продюсера.
+     * В этот экземпляр установлен топик по умолчанию.
+     */
     private KafkaTemplate<K,V> kafkaTemplate;
-
-    public CompletableFuture<SendResult<K,V>> send(K key, V value) {
-        return kafkaTemplate.sendDefault(key, value);
-    }
-
-    public final KafkaTemplate<K,V> getKafkaTemplate() {
-        return kafkaTemplate;
-    }
-
-    public final void setKafkaTemplate(KafkaTemplate<K,V> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         if (kafkaTemplate == null) {
             throw new IllegalStateException("kafkaTemplate must not be null");
         }
+    }
+
+    /**
+     * Отправляет сообщение в kafka.
+     *
+     * @param key ключ
+     * @param value значение
+     * @return результат отправки сообщения
+     *
+     * @see KafkaTemplate#sendDefault
+     */
+    public CompletableFuture<SendResult<K,V>> send(K key, V value) {
+        return kafkaTemplate.sendDefault(key, value);
+    }
+
+    /**
+     * Возвращает KafkaTemplate этого продюсера.
+     *
+     * @return KafkaTemplate этого продюсера
+     */
+    public final KafkaTemplate<K,V> getKafkaTemplate() {
+        return kafkaTemplate;
+    }
+
+    /**
+     * Устанавливает KafkaTemplate для этого продюсера.
+     */
+    public final void setKafkaTemplate(KafkaTemplate<K,V> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 }
