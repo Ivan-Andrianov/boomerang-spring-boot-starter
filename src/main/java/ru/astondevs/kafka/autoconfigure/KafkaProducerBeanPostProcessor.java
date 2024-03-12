cd2581e34f322df09c9ad3ad735edd77c2c64ec5
msyaskov <ms.yaskov@gmail.com>
2024-03-12 21:22:34 +0300

    docs: KafkaProducerBeanPostProcessor

--- src/main/java/ru/astondevs/kafka/autoconfigure/KafkaProducerBeanPostProcessor.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/KafkaProducerBeanPostProcessor.java
@@ -2,30 +2,50 @@ package ru.astondevs.kafka.autoconfigure;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.lang.NonNull;
 
+/**
+ * Пост процессор компонента, который связывает продюсера с соответствующим KafkaTemplate.
+ *
+ * @author Ivan Andrianov
+ * @author Maksim Yaskov
+ *
+ * @see KafkaProducer
+ * @see AbstractKafkaProducer
+ */
 @RequiredArgsConstructor
 public class KafkaProducerBeanPostProcessor implements BeanPostProcessor {
 
-    private final ApplicationContext context;
+    /**
+     * Фабрика компонентов.
+     */
+    private final ConfigurableListableBeanFactory beanFactory;
 
+    /**
+     * Свойства конфигураций.
+     */
     private final KafkaConfigurationProperties properties;
 
     @Override
     public Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
-        KafkaProducer annotation = context.findAnnotationOnBean(beanName, KafkaProducer.class);
-
-        if (annotation != null && bean instanceof AbstractKafkaProducer<?,?> producer) {
-
-            ProducerFactory producerFactory = ProducerFactoryFactory.createProducerFactory(properties.getProducers().get(annotation.value()));
-            KafkaTemplate kafkaTemplate = new KafkaTemplate<>(producerFactory);
-            kafkaTemplate.setDefaultTopic(properties.getProducers().get(annotation.value()).getTopic());
-            producer.setKafkaTemplate(kafkaTemplate);
+        KafkaProducer annotation = beanFactory.findAnnotationOnBean(beanName, KafkaProducer.class);
+        if (annotation == null) {
+            return bean;
         }
+
+        if (!(bean instanceof AbstractKafkaProducer<?,?> producer)) {
+            throw new BeanNotOfRequiredTypeException(beanName, AbstractKafkaProducer.class, bean.getClass());
+        }
+
+        ProducerFactory producerFactory = ProducerFactoryFactory.createProducerFactory(properties.getProducers().get(annotation.value()));
+        KafkaTemplate kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setDefaultTopic(properties.getProducers().get(annotation.value()).getTopic());
+        producer.setKafkaTemplate(kafkaTemplate);
 
         return bean;
     }
