20de83aac00d2c136341f9ed07ae1f5ef4a8c7a8
msyaskov <ms.yaskov@gmail.com>
2024-03-13 08:47:13 +0300

    add enabled property

--- src/main/java/ru/astondevs/kafka/autoconfigure/KafkaAutoConfiguration.java
+++ src/main/java/ru/astondevs/kafka/autoconfigure/KafkaAutoConfiguration.java
@@ -3,6 +3,7 @@ package ru.astondevs.kafka.autoconfigure;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import ru.astondevs.kafka.autoconfigure.annotation.KafkaConsumer;
@@ -18,6 +19,7 @@ import ru.astondevs.kafka.autoconfigure.producer.KafkaProducerBeanPostProcessor;
  */
 @AutoConfiguration
 @EnableConfigurationProperties(KafkaConfigurationProperties.class)
+@ConditionalOnProperty(prefix = "ru.astondevs.kafka", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class KafkaAutoConfiguration {
 
     /**
